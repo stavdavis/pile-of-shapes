8fd0f3440f71cc1fcda03f19db18b32ac690f306
Stav Davis <stav.davis@gmail.com>
2018-04-19 11:11:44 -0700

    corrected padding variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animate =   window.requestAnimationFrame || 
                 window.webkitRequestAnimationFrame || 
                 window.mozRequestAnimationFrame || 
-                function (callback) { window.setTimeout(callback, 1000 / 60) };
+                function (callback) { window.setTimeout(callback, 1000 / 60) }; 
 
 //defining the canvas area:
 var canvas = document.createElement("canvas");
